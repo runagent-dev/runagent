ec413c24ca57b07847ba577c69e6789a169cace1
sawradip <sawradip0@gmail.com>
2025-06-26 01:02:42 +0600

    Added fraemwork specific server structure

---
+++ runagent/sdk/server/framework/__init__.py
@@ -0,0 +1,14 @@
+from runagent.sdk.server.framework.langgraph import LangGraphExecutor
+from runagent.utils.schema import EntryPoint
+from pathlib import Path
+from typing import Dict
+
+
+def get_executor(agent_dir: Path, framework: str, agent_entrypoints: Dict[str, EntryPoint]):
+    if "generic" not in agent_entrypoints:
+        raise ValueError("User has not exposed a generic entrypoint for agent")
+    
+    if framework == "langgraph":
+        return LangGraphExecutor(agent_dir, agent_entrypoints)
+    else:
+        raise ValueError(f"Framework {framework} not supported yet.")
---
+++ runagent/sdk/server/framework/generic.py
@@ -0,0 +1,27 @@
+from runagent.utils.schema import RunAgentConfig
+from runagent.utils.imports import PackageImporter
+from pathlib import Path
+from typing import Dict
+from runagent.utils.schema import EntryPoint
+
+
+class GenericExecutor:
+    def __init__(self, agent_dir: Path, agent_entrypoints: Dict[str, EntryPoint]):
+        self.agent_dir = agent_dir
+        self.agent_entrypoints = agent_entrypoints
+
+        self.importer = PackageImporter(verbose=False)
+
+        self._generic_entrypoint = None
+
+        if "generic" in self.agent_entrypoints:
+            ep = self.agent_entrypoints["generic"]
+            self._generic_entrypoint = self.importer.resolve_import(
+                self.agent_dir / ep.file,
+                ep.module
+            )
+
+    def generic(self, *input_args, **input_kwargs):
+        if self._generic_entrypoint is None:
+            raise ValueError("No `generic` entrypoint found in agent config")
+        return self._generic_entrypoint(*input_args, **input_kwargs)
