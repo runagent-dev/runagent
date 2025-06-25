c7db71fdc4591aa7b95c5f3b6523c875b1ff2b60
sawradip <sawradip0@gmail.com>
2025-06-26 01:33:24 +0600

    test client

---
+++ runagent/test/client_test.py
@@ -0,0 +1,3 @@
+from runagent import RunAgentClient
+ra = RunAgentClient(agent_id="330489a3-ff37-46f3-b88b-432bce5000da")
+ra.run(input_args = [], input_kwargs={"query": "How to I fix my broken phone", "num_solutions": 2})
