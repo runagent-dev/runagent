39bb807071887e5b756af760a730c43b88489d17
sawradip <sawradip0@gmail.com>
2025-06-28 06:04:34 +0600

    Updated readme

--- runagent/test/client_test_async.py
+++ runagent/test/client_test_async.py
@@ -3,8 +3,6 @@ from runagent import AsyncRunAgentClient
 
 async def main():
     ra = AsyncRunAgentClient(agent_id="055b73d7-6239-4a94-a156-1193fcf33ff0")
-    # agent_results = await ra.run_generic(query="How to I fix my broken phone", num_solutions=2)
-    # print(agent_results)
 
     async for out in ra.run_generic_stream({
         "query": "How to I fix my broken phone?",
