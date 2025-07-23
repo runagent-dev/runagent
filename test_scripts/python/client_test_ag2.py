eff74c7ff24c6bd42c4f5bcc272b73d256982a68
RadeenXALNW <radeensust@gmail.com>
2025-07-23 16:11:01 +0000

    testing example with updated local db

--- test_scripts/python/client_test_ag2.py
+++ test_scripts/python/client_test_ag2.py
@@ -18,14 +18,16 @@
 from runagent import RunAgentClient
 
 ra = RunAgentClient(
-    agent_id="52f357ae-d4d8-4146-9ea5-8669a6091475",
-    entrypoint_tag="ag2_stream",
+    agent_id="ca7a82a2-70c4-4ded-b7e3-17d0a13f94cd",
+    entrypoint_tag="ag2_invoke",
     local=True
     )
 
-for chunk in ra.run(
-    message="The solar sytem has 2 planets.",
-    max_turns=3
-):
-    print(f"{chunk['sender']}: {chunk['content']}")
-    print("-------")
+print(ra.run(message="The solar sytem has 2 planets.", max_turns=3))
+
+# for chunk in ra.run(
+#     message="The solar sytem has 2 planets.",
+#     max_turns=3
+# ):
+#     print(f"{chunk['sender']}: {chunk['content']}")
+#     print("-------")
